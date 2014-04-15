9fe91ae99d3044616170b256c277a40c50e7226c
Ford <ford.peprah@uwaterloo.ca>
2014-04-14 22:40:45 -0400

    Fixed collectstatic.

--- gitachievements/settings/common.py
+++ gitachievements/settings/common.py
@@ -144,11 +144,8 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/'),
-)
 
 # Social Authentication Settings
 # Settings for the Django Pipeline
